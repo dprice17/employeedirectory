364c41e005645e32aa8f0b43d6e2f211c9d26263
dprice17 <109916870+dprice17@users.noreply.github.com>
2023-07-05 08:19:26 -0400

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import SearchResults from "./SearchResults.jsx";
+import Employees from "./Employees.js";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [employees, setEmployees] = useState(Employees);
+  const [employeeSearch, setEmployeeSearch] = useState("");
+  const [selectDepartment, setSelectDepartment] = useState("everyone");
+  const [employeeSearchMatches, setEmployeeSearchMatches] = useState([]);
+
+  const handleSelectDepartment = (event) => {
+    const selectedOption = event.target.value;
+    setSelectDepartment(selectedOption);
+  };
+
+  const handleEmployeeSearch = (event) => {
+    const searchName = event.target.value;
+    setEmployeeSearch(searchName);
+  };
+
+  useEffect(() => {
+    const matchingResults = employees.filter((employee) => {
+      const name = employee.name.toLowerCase();
+      const employeeSearched = employeeSearch.toLowerCase();
+      const employeeDepartment = employee.team;
+      const selectedDepartment = selectDepartment;
+
+      if (selectedDepartment === "everyone") {
+        return name.slice(0, employeeSearched.length) === employeeSearched;
+      }
+      return (
+        employeeDepartment === selectedDepartment &&
+        name.slice(0, employeeSearched.length) === employeeSearched
+      );
+    });
+
+    setEmployeeSearchMatches(matchingResults);
+  }, [employeeSearch, selectDepartment]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="container">
+      <header>
+        <div>
+          <h1>Meet the Team</h1>
+        </div>
+        <div className="search">
+          <select
+            className="search-dropdown-field"
+            onChange={handleSelectDepartment}
+          >
+            <option value="everyone">Everyone</option>
+            <option value="leadership">Leadership</option>
+            <option value="sales">Sales</option>
+            <option value="customer service">Customer Service</option>
+            <option value="product">Product</option>
+          </select>
+          <input
+            type="text"
+            placeholder="search name"
+            className="search-input-field"
+            onChange={handleEmployeeSearch}
+            value={employeeSearch}
+          />
+        </div>
+      </header>
 
-export default App
+      <SearchResults
+        employees={
+          employeeSearchMatches.length > 0 ? employeeSearchMatches : employees
+        }
+      />
+    </div>
+  );
+}
---
+++ src/SearchResults.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+//import employeeImages from "src/assets/";
+import twitterLogo from "/src/assets/twitter1.png";
+import linkdenLogo from "/src/assets/linkden1.png";
+
+export default function SearchResults(props) {
+  const { employees } = props;
+
+  const handleEmployeeProfileRendering = () => {
+    const employeeProfiles = employees.map((employee) => {
+      const employeeBioParagraphs = employee.bio
+        .split("</p>")
+        .filter((sentence) => sentence.trim() !== "")
+        .map((sentence, index) => (
+          <p key={index}>{sentence.replace(/<p>/g, "")}</p>
+        ));
+
+      return (
+        <div className="employee-profile" key={uuidv4()}>
+          <img alt="Employee" />
+          <div className="employee-profile-name-title-container">
+            <p>{employee.name}</p>
+            <p>{employee.title}</p>
+          </div>
+          <div className="employee-profile-bio">{employeeBioParagraphs}</div>
+          <div className="social-icons-container">
+            {employee.social.twitter && (
+              <a href="">
+                <img src={twitterLogo} alt="Twitter" />
+              </a>
+            )}
+            {employee.social.linkedin && (
+              <a href="#">
+                <img src={linkdenLogo} alt="LinkedIn" />
+              </a>
+            )}
+          </div>
+        </div>
+      );
+    });
+    return employeeProfiles;
+  };
+
+  return (
+    <div className="employee-results-container">
+      {handleEmployeeProfileRendering()}
+    </div>
+  );
+}
