2c2522bd43f08233d6f11f7e78f2f71a521a1ccc
dprice17 <109916870+dprice17@users.noreply.github.com>
2023-07-10 07:43:04 -0400

    updated header styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import SearchResults from "./SearchResults.jsx";
 import Employees from "./Employees.js";
+import headerBackground from "/src/assets/header-bg-1.png";
 
 export default function App() {
   const [employees, setEmployees] = useState(Employees);
@@ -37,9 +38,22 @@ export default function App() {
     setEmployeeSearchMatches(matchingResults);
   }, [employeeSearch, selectDepartment]);
 
+  const headerStyles = {
+    background: `linear-gradient(
+      180deg,
+      #319e97 0%,
+      rgba(49, 158, 151, 0) 59.9%,
+      rgba(49, 158, 151, 0) 100%
+    ),
+    url(${headerBackground})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  };
+
   return (
     <div className="container">
-      <header>
+      <header style={headerStyles}>
         <div>
           <h1>Meet the Team</h1>
         </div>
